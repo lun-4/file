f600cf9d09e53b44b34a772e9b4c17c623433b24
Luna <git@l4.pm>
2024-08-06 23:45:29 -0300

    fix macos
    it already has strlcpy, no need to add it

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -11,30 +11,35 @@ pub fn build(b: *std.Build) void {
     });
     lib.linkLibC();
     lib.addIncludePath(b.path("luna-config"));
+    // macos has strlcpy
+    var c_files = std.ArrayList([]const u8).init(b.allocator);
+    for ([_][]const u8{
+        "src/buffer.c",
+        "src/magic.c",
+        "src/apprentice.c",
+        "src/softmagic.c",
+        "src/ascmagic.c",
+        "src/encoding.c",
+        "src/compress.c",
+        "src/is_csv.c",
+        "src/is_json.c",
+        "src/is_tar.c",
+        "src/readelf.c",
+        "src/print.c",
+        "src/fsmagic.c",
+        "src/funcs.c",
+        "src/apptype.c",
+        "src/der.c",
+        "src/cdf.c",
+        "src/cdf_time.c",
+        "src/readcdf.c",
+        "src/fmtcheck.c",
+    }) |c_file| try c_files.append(c_file);
+    if (target.result.os.tag != .macos) {
+        try c_files.append("src/strlcpy.c");
+    }
     lib.addCSourceFiles(.{
-        .files = &.{
-            "src/buffer.c",
-            "src/magic.c",
-            "src/apprentice.c",
-            "src/softmagic.c",
-            "src/ascmagic.c",
-            "src/encoding.c",
-            "src/compress.c",
-            "src/is_csv.c",
-            "src/is_json.c",
-            "src/is_tar.c",
-            "src/readelf.c",
-            "src/print.c",
-            "src/fsmagic.c",
-            "src/funcs.c",
-            "src/apptype.c",
-            "src/der.c",
-            "src/cdf.c",
-            "src/cdf_time.c",
-            "src/readcdf.c",
-            "src/fmtcheck.c",
-            "src/strlcpy.c",
-        },
+        .files = c_files.items,
         .flags = &.{"-DHAVE_CONFIG_H=1"},
     });
     lib.addIncludePath(b.path("."));
